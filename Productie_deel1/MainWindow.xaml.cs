d6afe6108497f9b1e90f275725e4b72df2a0c31b
ozturkbatuhann <r1036481@student.vives.be>
2025-05-21 18:16:24 +0200

    project added

---
+++ Productie_deel1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace Productie_deel1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // list om producten bij te houden
+        private List<Product> producten = new List<Product>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoegToe_Click(object sender, RoutedEventArgs e)
+        {
+            VoegToe();
+        }
+
+        // methode om een nieuuw product toe te voegen
+        private void VoegToe()
+        {
+            
+            if (string.IsNullOrWhiteSpace(txtProduct.Text) || string.IsNullOrWhiteSpace(txtHoeveelheid.Text))
+            {
+                MessageBox.Show("Gelieve alle velden in te vullen.", "Fout", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            
+            if (!int.TryParse(txtHoeveelheid.Text, out int hoeveelheid))
+            {
+                MessageBox.Show("Hoeveelheid moet een geheel getal zijn.", "Fout", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // mmaak een nieuw product 
+            Product nieuwProduct = new Product(txtProduct.Text, hoeveelheid);
+            producten.Add(nieuwProduct);
+
+            // toon de gegevens 
+            lstProducten.Items.Add(nieuwProduct.Gegevens());
+
+            // maak de tekstvelden
+            txtProduct.Clear();
+            txtHoeveelheid.Clear();
+        }
+    }
+}
---
+++ Productie_deel1/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productie_deel1
+{
+    public class Product
+    {
+        private string _sNaam;
+        private int _iHoeveelheid;
+
+        public Product(string naam, int hoeveelheid)
+        {
+            _sNaam = naam;
+            _iHoeveelheid = hoeveelheid;
+        }
+
+        public string Gegevens()
+        {
+            return $"Product: {_sNaam} - Hoeveelheid: {_iHoeveelheid}";
+        }
+
+        public string Naam
+        {
+            get { return _sNaam; }
+        }
+    }
+}
